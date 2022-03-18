2495ad7443fccbfceda9b83d60118a70f0b4e0cf
Adrian Murage <adrianmurage21@gmail.com>
2022-03-18 16:05:16 +0300

    working with props

--- src/components/AnimalDataDisplay/AnimalCard.js
+++ src/components/AnimalDataDisplay/AnimalCard.js
@@ -1,6 +1,22 @@
 import React from "react";
-import "./AnimalCard.css"
+import "./AnimalCard.css";
 
-export default function AnimalCard(){
-    return <h3>Animal</h3>
+export default function AnimalCard({
+  additional,
+  diet,
+  key,
+  name,
+  scientificName,
+  showAdditional,
+  size,
+}) {
+  return (
+    <div key={key}>
+      <h3>{name}</h3>
+      <h4>{scientificName}</h4>
+      <h5>{size} kg</h5>
+      <div>{diet.join(", ")}.</div>
+      <button onClick={() => showAdditional(additional)}>More Info</button>
+    </div>
+  );
 }
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,13 @@ import EmojiDisplay from "../EmojiDisplay/EmojiDisplay";
 import AnimalCard from "../AnimalDataDisplay/AnimalCard";
 import data from "../AnimalDataDisplay/data";
 
+function showAdditional(additional) {
+  const alertInformation = Object.entries(additional)
+    .map((information) => `${information[0]}: ${information[1]}`)
+    .join("\n");
+  alert(alertInformation);
+}
+
 function App() {
   return (
     <>
@@ -15,7 +22,15 @@ function App() {
       <div className="animals-wrapper">
         <h2>Animals</h2>
         {data.map((animal) => (
-          <AnimalCard key={animal.name} />
+          <AnimalCard
+            additional={animal.additional}
+            diet={animal.diet}
+            key={animal.name}
+            name={animal.name}
+            scientificName={animal.scientificName}
+            showAdditional={showAdditional}
+            size={animal.size}
+          />
         ))}
       </div>
     </>

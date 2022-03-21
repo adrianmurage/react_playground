9d4b919bf55b2de56c5274405fba0b6331b82c51
Adrian Murage <adrianmurage21@gmail.com>
2022-03-21 12:40:58 +0300

    create CreatingWrappers component

---
+++ src/components/CreatingWrappers/CreatingWrappers.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import styles from "./CreatingWrappers.module.css";
+// import propType from "prop-types";
+
+export default function CreatingWrappers({ name }) {
+  return (
+    <>
+      <p>I am here</p>
+      <div>{name}</div>
+      {/* <div>{diet}</div>
+      <div>{size}</div> */}
+    </>
+  );
+}
+
+// CreatingWrappers.propType(
+
+// )
